bcd193f8969947bb339a1980f888d569899f2099
kho903 <gmldnr2222@naver.com>
2020-08-08 17:04:27 +0900

    BodyParser, PostMan, 회원가입

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kho903:<password>@boilerplate.rnzb5.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://kho903:64033902@boilerplate-shard-00-00.rnzb5.mongodb.net:27017,boilerplate-shard-00-01.rnzb5.mongodb.net:27017,boilerplate-shard-00-02.rnzb5.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-fogjd0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected ... '))
     .catch(err => console.log(err))
@@ -13,5 +17,18 @@ mongoose.connect('mongodb+srv://kho903:<password>@boilerplate.rnzb5.mongodb.net/
 
 
 app.get('/', (req, res) => res.send('Hello World! 안녕하세요!'))
+
+app.post('/register', (req, res) => {
+    // 회원가입 할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터베이스에 넣어준다.
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
